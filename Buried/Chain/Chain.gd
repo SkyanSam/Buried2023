d694630ac057ac9b627e3796889ce963db3869d0
SkyanSam <samuelwiggins15@gmail.com>
2023-09-16 17:13:15 -0400

    player can follow chain in samplescene "game.tscn"
    NOTE to self: work on parrying chains next and move chain to DepressionBoss scene

--- Buried/Chain/Chain.gd
+++ Buried/Chain/Chain.gd
@@ -2,7 +2,7 @@ extends Line2D
 
 var chain_speed = 500;
 var chain_origin : Vector2 = Vector2(0,0)
-var chain_target : Vector2 = Vector2(1200,700)
+export var chain_target_path : NodePath
 
 func _ready():
 	points.resize(2)
@@ -10,6 +10,7 @@ func _ready():
 	points[0] = chain_origin;
 
 func _process(delta):
+	var chain_target = (get_node(chain_target_path) as Node2D).global_position
 	points[0] = points[0].move_toward(chain_target, delta * chain_speed)
 	$ChainEnd.position = points[0] # check if it needs to be global
 	$RayCast2D.position = points[1]
