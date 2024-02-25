47115d8d0c940ba073f96b34ebe53124c7b5b052
SkyanSam <samuelwiggins15@gmail.com>
2024-02-24 20:03:23 -0500

    infinite chains glitch

--- Buried/Chain/Chain.gd
+++ Buried/Chain/Chain.gd
@@ -3,17 +3,25 @@ extends Line2D
 var chain_speed = 500;
 var chain_origin : Vector2 = Vector2(0,0)
 export var chain_target_path : NodePath
+var chain_end_pos : Vector2
 enum ChainMode { Ejecting, Returning }
 var chain_mode : int = ChainMode.Ejecting
-func _ready():
+func _enter_tree():
+	# Setting the points for the chain
 	points.resize(2)
-	points[0] = chain_origin;
-	points[0] = chain_origin;
+	points[0] = chain_origin
+	points[1] = chain_origin
+	# Getting the position we want the chain to end at
+	var chain_target_node = get_node(chain_target_path) as Node2D
+	print(chain_target_path)
+	print(chain_target_node)
+	chain_end_pos = chain_target_node.global_position
 
 func _process(delta):
+	# Seeing if we need to eject the chain or retract the chain from the end position
 	var chain_target : Vector2
 	if (chain_mode == ChainMode.Ejecting):
-		chain_target = (get_node(chain_target_path) as Node2D).global_position
+		chain_target = chain_end_pos
 	else:
 		chain_target = chain_origin
 	points[0] = points[0].move_toward(chain_target, delta * chain_speed)
--- Buried/DepressionBoss/DepressionBoss.gd
+++ Buried/DepressionBoss/DepressionBoss.gd
@@ -4,29 +4,25 @@ const Chain = preload("res://Chain/Chain.gd")
 
 export var chain_prefab : PackedScene
 export var chain_target_path : NodePath
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+
 export var total_number_of_chains = 0
 var current_number_of_chains = 0
 var current_chains : Array
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+
 
 
 func _on_Timer_timeout():
 	if (current_number_of_chains < total_number_of_chains):
+		# Create a chain
 		var chain = chain_prefab.instance()
-		get_tree().get_root().add_child(chain)
+		# Set chain variables
 		chain.chain_origin = global_position
 		chain.chain_target_path = chain_target_path
+		# Add chain to list
 		current_chains.append(chain)
+		get_parent().add_child(chain)
 	else:
+		# Return all chains once the max # of chains is reached
 		for i in current_chains:
 			current_chains[i].chain_mode = Chain.ChainMode.Returning
 			# Note need to free chains here or in Chain.gd
