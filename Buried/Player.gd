6f23fd57d526494512350a57d840ab4730ac983c
XaelZeph <89801025+XaelZeph@users.noreply.github.com>
2024-07-07 13:09:06 -0400

    Added holding jump
    The jump is now able to be held and released in order to have a shorter jump

--- Buried/Player.gd
+++ Buried/Player.gd
@@ -1,17 +1,20 @@
 extends KinematicBody2D
 
 export (int) var speed = 1200
-export (int) var jump_speed = -1800
+export (int) var jump_speed = -3000
 export (int) var gravity = 4000
 export (int) var falling_gravity = 6000;
+#later apply a timer so that if the character is slightly off the ledge, they can still jump
 
 var velocity = Vector2.ZERO
 
 func get_input():
 	velocity.x = 0
+	#if ui_right is pressed, velocity is increased to the right and sprite is flipped
 	if Input.is_action_pressed("ui_right"):
 		velocity.x += speed
 		$AnimatedSprite.flip_h = false
+	#if ui_right is pressed, velocity is increased to the left and sprite is flipped
 	if Input.is_action_pressed("ui_left"):
 		velocity.x -= speed
 		$AnimatedSprite.flip_h = true
@@ -28,5 +31,7 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("ui_up"):
 		if is_on_floor():
 			velocity.y = jump_speed
-			
+	if Input.is_action_just_released("ui_up"):
+		if velocity.y < 0.0:
+			velocity.y *= .2
 
