79ca4e4091628fd39c4e43ec030fb8aa18d3b7bd
SkyanSam <samuelwiggins15@gmail.com>
2024-07-14 11:50:16 -0400

    enemy.gd 4.2.2 update

--- Buried/Enemy.gd
+++ Buried/Enemy.gd
@@ -1,24 +1,24 @@
 extends CharacterBody2D
 
-@export (int) var speed = 1200
-@export (int) var jump_speed = -1800
-@export (int) var gravity = 4000
-@export (Vector2) var knockback = Vector2.ONE
+@export var speed: int = 1200
+@export var jump_speed: int = -1800
+@export var gravity: int = 4000
+@export var knockback: Vector2 = Vector2.ONE
 
-var velocity = Vector2.ZERO
+var vel : Vector2 = Vector2.ZERO
 
 
 func _physics_process(delta):
-	velocity.y += gravity * delta
-	set_velocity(velocity)
+	vel.y += gravity * delta
+	set_velocity(vel)
 	set_up_direction(Vector2.UP)
 	move_and_slide()
-	velocity = velocity
+	velocity = vel
 	if is_on_floor():
-		velocity = knockback
+		vel = knockback
 	if Input.is_action_just_pressed("ui_up"):
 		if is_on_floor():
-			velocity.y = jump_speed
+			vel.y = jump_speed
 
 func do_knockback():
-	velocity = knockback
+	vel = knockback
