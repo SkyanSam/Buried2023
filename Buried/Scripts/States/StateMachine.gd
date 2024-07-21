7fa41b2dd910513b94ec5668dbc7f43332379c4a
XaelZeph <89801025+XaelZeph@users.noreply.github.com>
2024-07-21 13:52:26 -0400

    fixed some stuff in statemachine.gd
    things weren't saved, so added the code back

--- Buried/Scripts/States/StateMachine.gd
+++ Buried/Scripts/States/StateMachine.gd
@@ -1,5 +1,7 @@
 extends Node
 
+@export var initial_state : State
+
 var current_state : State
 var states : Dictionary = {}
 
@@ -8,6 +10,9 @@ func _ready():
 		if child is State:
 			states[child.name.to_lower()] = child
 			child.Transitioned.connect(on_child_transition)
+	if initial_state:
+		initial_state.Enter()
+		current_state = initial_state
 
 func _process(delta:):
 	if current_state:
@@ -26,6 +31,6 @@ func on_child_transition(state, new_state_name):
 		return
 	
 	if current_state:
-		current_state.exit()
+		current_state.Exit()
 	
-	new_state.enter()
+	new_state.Enter()
